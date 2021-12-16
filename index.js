1d0d293c2e3f238cf3bc8d2bf7a1a6ddca424b67
David Puerta Martos <dpuerta@usal.es>
2021-12-16 20:58:06 +0100

    Created logic for game over

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const gameboard = {
-    state: [["E", "E", "E"],["E", "E", "E"],["E", "E", "E"]]
+    state: [["E", "E", "E"],["E", "E", "E"],["E", "E", "E"]],
 }
 
 const controller = (()=>{
+
     const createNewDiv = (rowIndex) => {
         const newDiv = document.createElement("div")
         newDiv.setAttribute("index", rowIndex)
@@ -27,9 +28,33 @@ const controller = (()=>{
         return newRowDiv
     }
     
-    const createMark = (span) =>{
-        if(span.textContent==="X"){return ;}
-        else{span.textContent ="X"}
+    const gameChecker = () => {
+        squares = document.querySelectorAll(".square")
+        let array = []
+        squares.forEach(square => array = array.concat(square.textContent))
+        console.log(array)
+        // first check horizontals and verticals
+        for(let i=0; i<array.length;i=i+3){
+            let row = array.slice(i,i+3)
+            let column = [array[i],array[i+3],array[i+6]]
+            if(row.every(e=>e==="X") || column.every(e=>e==="X")){
+                return console.log("tim wins")
+            }
+            else if(row.every(e=>e==="O") || column.every(e=>e==="O")){
+                return console.log("jim wins")
+            }
+        }
+        // then check diagonals
+        let diag1 = [array[0],array[4],array[8]]
+        let diag2 = [array[2],array[4],array[6]]
+        console.log("diag1", diag1, "diag2", diag2)
+        if(diag1.every(e=>e==="X") || diag2.every(e=>e==="X")){
+            return console.log("tim wins")
+        }
+        else if(diag1.every(e=>e==="O") || diag2.every(e=>e==="O")){
+            console.log("jim wins")
+            return console.log("jim wins")
+        }
     }
     
     // Create Board
@@ -42,7 +67,18 @@ const controller = (()=>{
         })
         squares = document.querySelectorAll(".square")
         squares.forEach(square=>square.addEventListener("click", ()=>{
-            createMark(square)
+            if(jim.isTurn){
+                jim.createMark(square)
+                jim.isTurn = !jim.isTurn
+                tim.isTurn = !tim.isTurn
+                gameChecker()
+            }
+            else{
+                tim.createMark(square)
+                jim.isTurn = !jim.isTurn
+                tim.isTurn = !tim.isTurn
+                gameChecker()
+            }
             console.log(square)
         }))
     }
@@ -52,18 +88,22 @@ const controller = (()=>{
 
 })();
 
-const player = (name, id) => {
+const Player = (name, id1) => {
     let isTurn = false
-    let id = id
+    let id = id1
     const changeTurn = () => isTurn = !isTurn
     const createMark = (span) =>{
+        console.log(span.textContent, "id", id)
         if(span.textContent!="E"){return ;}
-        else if(id===1){span.textContent==="O"}
-        else{span.textContent==="X"}
+        else if(id===1){span.textContent="O"}
+        else{span.textContent="X"}
     }
-    return {changeTurn, createMark}
+    return {isTurn, changeTurn, createMark}
 }
 
 
 controller.createBoard()
+const jim = Player("jim", 1)
+const tim = Player("tim", 2)
+jim.isTurn=true
 console.log(gameboard.state)
