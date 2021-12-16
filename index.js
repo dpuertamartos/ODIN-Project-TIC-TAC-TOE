4713756d3f24b4cef21647d51d11ff1724915eae
David Puerta Martos <dpuerta@usal.es>
2021-12-16 20:05:57 +0100

    player factory function created

--- index.js
+++ index.js
@@ -1,59 +1,69 @@
 const gameboard = {
-    state: [["0", "0", "0"],["0", "0", "0"],["0", "0", "0"]]
+    state: [["E", "E", "E"],["E", "E", "E"],["E", "E", "E"]]
 }
 
-const controller = {}
-
-const player = (name) => {
-    return ;
-}
-
-const createNewDiv = (rowIndex) => {
-    const newDiv = document.createElement("div")
-    newDiv.setAttribute("index", rowIndex)
-    return newDiv
-}
-
-const createNewSpan = (textContent) => {
-    const newSpan = document.createElement("span")
-    newSpan.textContent = textContent
-    return newSpan
-}
-
-const createNewRow = (row, rowIndex) => {
-    const newRowDiv = createNewDiv(rowIndex)
-    for(let i=0; i<row.length; i++){
-        const newSquareSpan = createNewSpan(row[i])
-        newSquareSpan.setAttribute("row", rowIndex)
-        newSquareSpan.setAttribute("column", i)
-        newSquareSpan.className="square"
-        newRowDiv.append(newSquareSpan)
+const controller = (()=>{
+    const createNewDiv = (rowIndex) => {
+        const newDiv = document.createElement("div")
+        newDiv.setAttribute("index", rowIndex)
+        return newDiv
     }
-    return newRowDiv
+    
+    const createNewSpan = (textContent) => {
+        const newSpan = document.createElement("span")
+        newSpan.textContent = textContent
+        return newSpan
+    }
+    
+    const createNewRow = (row, rowIndex) => {
+        const newRowDiv = createNewDiv(rowIndex)
+        for(let i=0; i<row.length; i++){
+            const newSquareSpan = createNewSpan(row[i])
+            newSquareSpan.setAttribute("row", rowIndex)
+            newSquareSpan.setAttribute("column", i)
+            newSquareSpan.className="square"
+            newRowDiv.append(newSquareSpan)
+        }
+        return newRowDiv
+    }
+    
+    const createMark = (span) =>{
+        if(span.textContent==="X"){return ;}
+        else{span.textContent ="X"}
+    }
+    
+    // Create Board
+    const createBoard = () => {
+        gameDiv = document.querySelector(".boardContainer")
+        gameboard.state.map(row=>{
+        const rowIndex = gameboard.state.indexOf(row)
+        const newRowDiv = createNewRow(row, rowIndex)
+        gameDiv.append(newRowDiv)
+        })
+        squares = document.querySelectorAll(".square")
+        squares.forEach(square=>square.addEventListener("click", ()=>{
+            createMark(square)
+            console.log(square)
+        }))
+    }
+    return {
+        createBoard
+    }
+
+})();
+
+const player = (name, id) => {
+    let isTurn = false
+    let id = id
+    const changeTurn = () => isTurn = !isTurn
+    const createMark = (span) =>{
+        if(span.textContent!="E"){return ;}
+        else if(id===1){span.textContent==="O"}
+        else{span.textContent==="X"}
+    }
+    return {changeTurn, createMark}
 }
 
-const createMark = (span) =>{
-    span.textContent = "X"
-}
 
-
-// Create Board
-const createBoard = () => {
-    gameDiv = document.querySelector(".boardContainer")
-    gameboard.state.map(row=>{
-    const rowIndex = gameboard.state.indexOf(row)
-    const newRowDiv = createNewRow(row, rowIndex)
-    gameDiv.append(newRowDiv)
-    })
-    squares = document.querySelectorAll(".square")
-    squares.forEach(square=>square.addEventListener("click", ()=>{
-        createMark(square)
-        console.log(square)
-    }))
-}
-
-
-
-
-createBoard()
+controller.createBoard()
 console.log(gameboard.state)
