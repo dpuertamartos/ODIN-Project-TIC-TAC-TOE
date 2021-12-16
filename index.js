3659fbd1c4642c91bd6536c5edd058b044c3d965
David Puerta Martos <dpuerta@usal.es>
2021-12-16 19:39:17 +0100

    create Mark functionality added

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const player = (name) => {
     return ;
 }
 
-const createNewDiv = (textContent) => {
+const createNewDiv = (rowIndex) => {
     const newDiv = document.createElement("div")
-    newDiv.textContent = textContent
+    newDiv.setAttribute("index", rowIndex)
     return newDiv
 }
 
@@ -20,21 +20,40 @@ const createNewSpan = (textContent) => {
     return newSpan
 }
 
-const createNewRow = (row) => {
-    const newRowDiv = createNewDiv()
+const createNewRow = (row, rowIndex) => {
+    const newRowDiv = createNewDiv(rowIndex)
     for(let i=0; i<row.length; i++){
         const newSquareSpan = createNewSpan(row[i])
+        newSquareSpan.setAttribute("row", rowIndex)
+        newSquareSpan.setAttribute("column", i)
+        newSquareSpan.className="square"
         newRowDiv.append(newSquareSpan)
     }
     return newRowDiv
 }
 
+const createMark = (span) =>{
+    span.textContent = "X"
+}
+
+
 // Create Board
-
-gameDiv = document.querySelector(".boardContainer")
-gameboard.state.map(row=>{
-    const newRowDiv = createNewRow(row)
+const createBoard = () => {
+    gameDiv = document.querySelector(".boardContainer")
+    gameboard.state.map(row=>{
+    const rowIndex = gameboard.state.indexOf(row)
+    const newRowDiv = createNewRow(row, rowIndex)
     gameDiv.append(newRowDiv)
-})
+    })
+    squares = document.querySelectorAll(".square")
+    squares.forEach(square=>square.addEventListener("click", ()=>{
+        createMark(square)
+        console.log(square)
+    }))
+}
 
+
+
+
+createBoard()
 console.log(gameboard.state)
