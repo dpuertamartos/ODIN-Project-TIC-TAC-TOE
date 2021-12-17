ad77b3a846beeaacd49ad282c76ea6a1ccdc8852
David Puerta Martos <dpuerta@usal.es>
2021-12-17 21:12:59 +0100

    AI INVENCIBLE implemented

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const gameboard = {
     state: [["E", "E", "E"],["E", "E", "E"],["E", "E", "E"]],
     aiMode: true,
     aiTurn: true,
-    winner: ""
+    winner: "",
+    aiInvencible: true,
 }
 
 const controller = (()=>{
@@ -32,6 +33,20 @@ const controller = (()=>{
         return newRowDiv
     }
     
+    const createGameBoardState = () => {
+        squares = document.querySelectorAll(".square")
+        let array = []
+        let newState = []
+        squares.forEach(square => array = array.concat(square.textContent))
+        for(let i=0; i<array.length;i=i+3){
+            let row = array.slice(i,i+3)
+            newState.push(row)
+        }
+        gameboard.state= newState
+        console.log(gameboard.state)
+        return gameboard.state
+    }
+
     const gameChecker = (p1,p2) => {
         squares = document.querySelectorAll(".square")
         let array = []
@@ -91,7 +106,7 @@ const controller = (()=>{
             tim.isTurn = !tim.isTurn
             gameboard.aiTurn = !gameboard.aiTurn
             gameChecker(jim,tim)
-            ai.random()
+            ai.playBestMove()
         }
     }
 
@@ -139,7 +154,7 @@ const controller = (()=>{
 
     
     return {
-        createBoard, startGame
+        createBoard, startGame, createGameBoardState
     }
 
 })();
@@ -181,8 +196,235 @@ const ai = (() => {
         }
     }
 
-    const brained = () => {return ;}
-    return {random}
+    class Move
+    {
+        constructor()
+        {
+            let row,col;
+        }
+    }
+     
+    let player = 'O', opponent = 'X';
+     
+    // This function returns true if there are moves
+    // remaining on the board. It returns false if
+    // there are no moves left to play.
+    function isMovesLeft(board)
+    {
+        for(let i = 0; i < 3; i++)
+            for(let j = 0; j < 3; j++)
+                if (board[i][j] == 'E')
+                    return true;
+                     
+        return false;
+    }
+     
+    // This is the evaluation function as discussed
+    // in the previous article ( http://goo.gl/sJgv68 )
+    function evaluate(b)
+    {
+         
+        // Checking for Rows for X or O victory.
+        for(let row = 0; row < 3; row++)
+        {
+            if (b[row][0] == b[row][1] &&
+                b[row][1] == b[row][2])
+            {
+                if (b[row][0] == player)
+                    return +10;
+                     
+                else if (b[row][0] == opponent)
+                    return -10;
+            }
+        }
+      
+        // Checking for Columns for X or O victory.
+        for(let col = 0; col < 3; col++)
+        {
+            if (b[0][col] == b[1][col] &&
+                b[1][col] == b[2][col])
+            {
+                if (b[0][col] == player)
+                    return +10;
+      
+                else if (b[0][col] == opponent)
+                    return -10;
+            }
+        }
+      
+        // Checking for Diagonals for X or O victory.
+        if (b[0][0] == b[1][1] && b[1][1] == b[2][2])
+        {
+            if (b[0][0] == player)
+                return +10;
+                 
+            else if (b[0][0] == opponent)
+                return -10;
+        }
+      
+        if (b[0][2] == b[1][1] &&
+            b[1][1] == b[2][0])
+        {
+            if (b[0][2] == player)
+                return +10;
+                 
+            else if (b[0][2] == opponent)
+                return -10;
+        }
+      
+        // Else if none of them have
+        // won then return 0
+        return 0;
+    }
+     
+    // This is the minimax function. It
+    // considers all the possible ways
+    // the game can go and returns the
+    // value of the board
+    function minimax(board, depth, isMax)
+    {
+        let score = evaluate(board);
+      
+        // If Maximizer has won the game
+        // return his/her evaluated score
+        if (score == 10)
+            return score;
+      
+        // If Minimizer has won the game
+        // return his/her evaluated score
+        if (score == -10)
+            return score;
+      
+        // If there are no more moves and
+        // no winner then it is a tie
+        if (isMovesLeft(board) == false)
+            return 0;
+      
+        // If this maximizer's move
+        if (isMax)
+        {
+            let best = -1000;
+      
+            // Traverse all cells
+            for(let i = 0; i < 3; i++)
+            {
+                for(let j = 0; j < 3; j++)
+                {
+                     
+                    // Check if cell is empty
+                    if (board[i][j]=='E')
+                    {
+                         
+                        // Make the move
+                        board[i][j] = player;
+      
+                        // Call minimax recursively
+                        // and choose the maximum value
+                        best = Math.max(best, minimax(board,
+                                        depth + 1, !isMax));
+      
+                        // Undo the move
+                        board[i][j] = 'E';
+                    }
+                }
+            }
+            return best;
+        }
+      
+        // If this minimizer's move
+        else
+        {
+            let best = 1000;
+      
+            // Traverse all cells
+            for(let i = 0; i < 3; i++)
+            {
+                for(let j = 0; j < 3; j++)
+                {
+                     
+                    // Check if cell is empty
+                    if (board[i][j] == 'E')
+                    {
+                         
+                        // Make the move
+                        board[i][j] = opponent;
+      
+                        // Call minimax recursively and
+                        // choose the minimum value
+                        best = Math.min(best, minimax(board,
+                                        depth + 1, !isMax));
+      
+                        // Undo the move
+                        board[i][j] = 'E';
+                    }
+                }
+            }
+            return best;
+        }
+    }
+     
+    // This will return the best possible
+    // move for the player
+    function findBestMove(board)
+    {
+        let bestVal = -1000;
+        let bestMove = new Move();
+        bestMove.row = -1;
+        bestMove.col = -1;
+      
+        // Traverse all cells, evaluate
+        // minimax function for all empty
+        // cells. And return the cell
+        // with optimal value.
+        for(let i = 0; i < 3; i++)
+        {
+            for(let j = 0; j < 3; j++)
+            {
+                 
+                // Check if cell is empty
+                if (board[i][j] == 'E')
+                {
+                     
+                    // Make the move
+                    board[i][j] = player;
+      
+                    // compute evaluation function
+                    // for this move.
+                    let moveVal = minimax(board, 0, false);
+      
+                    // Undo the move
+                    board[i][j] = 'E';
+      
+                    // If the value of the current move
+                    // is more than the best value, then
+                    // update best
+                    if (moveVal > bestVal)
+                    {
+                        bestMove.row = i;
+                        bestMove.col = j;
+                        bestVal = moveVal;
+                    }
+                }
+            }
+        }
+      
+        return bestMove;
+    }
+
+    playBestMove = () => {
+        let board = controller.createGameBoardState()
+        console.log(board)
+        const moveToPlay = findBestMove(board)
+        console.log(moveToPlay)
+        if(gameboard.aiTurn === true && gameboard.winner.length < 1 ){
+            console.log(moveToPlay, moveToPlay.row, moveToPlay.col, typeof moveToPlay.col)
+            let square = document.querySelector(`[row="${moveToPlay.row}"][column="${moveToPlay.col}"]`)
+            console.log(square, "AI PLAY")
+            square.click("AI")
+            gameboard.aiTurn = false
+        }
+    }
+    return {random, findBestMove, playBestMove}
 })();
 
 
@@ -190,7 +432,17 @@ const ai = (() => {
 const start = document.querySelector(".startButton")
 start.addEventListener("click", ()=>{
     controller.startGame()
-    ai.random()
+    ai.playBestMove()
+})
+
+const boardState = document.querySelector(".boardState")
+boardState.addEventListener("click", ()=>{
+    controller.createGameBoardState()
+})
+
+const bestMove = document.querySelector(".bestMove")
+bestMove.addEventListener("click", ()=>{
+    console.log(ai.findBestMove(controller.createGameBoardState()))
 })
 
 console.log(gameboard.state)
